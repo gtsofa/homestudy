4ec7f248681ca5ddc43068b8b42a1a047af62f31
gtsofa <jlsnyule@yahoo.com>
2018-03-12 23:09:36 +0300

    add notes

--- fraction.py
+++ fraction.py
@@ -1,5 +1,18 @@
 # class that model a Fraction
 
+"""
+Notice that the formal parameter list contains three items (self, top, bottom). 
+self is a special parameter that will always be used as a reference back to the object itself.
+ It must always be the first formal parameter; however, it will never be given 
+ an actual parameter value upon invocation. As described earlier, fractions require two pieces of 
+ state data, the numerator and the denominator
+
+ The notation self.num in the constructor defines the fraction object to have an internal
+  data object called num as part of its state. Likewise, self.den creates the denominator. 
+  The values of the two formal parameters are initially assigned to the state, allowing the 
+  new fraction object to know its starting value.
+"""
+
 class Fraction:
 
     def __init__(self, num, deno):
