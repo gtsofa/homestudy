818f3f1e9defd2794a122cd231b186d4a6f2f1e2
gtsofa <jlsnyule@yahoo.com>
2018-03-13 06:21:17 +0300

    add find min element in a list

---
+++ find_min_element.py
@@ -0,0 +1,11 @@
+# find the minimum element in a list
+
+"""
+example: alist = [5,4,2,1,0]
+steps:
+-minelement == first element in a list when sorted( ascending order)
+-overallmin = alist[0]
+-loop/iterate to check for smallest element
+-loop/iterate again to compare first smallest element to 2nd element in the item
+-if true return the overallmin element
+"""
