75372fed39e8209bbe20c66be6c7050042cb1dba
Julius Nyule <jlsnyule@yahoo.com>
2018-03-13 08:34:24 +0300

    Update find_min_element.py
    added implemented code for the steps above

--- find_min_element.py
+++ find_min_element.py
@@ -8,8 +8,22 @@ steps:
 -loop/iterate through the list and check if the first element is the min/smallest
 -loop/iterate again through the list and compare that value to the next 2nd element in the list
 -if i > j : then i[that value] is not the smallest
+-return either true/false for the 2nd loop
 -done with the two loops 
 -return the result
 -
 -if true return the overallmin element
 """
+def findMin(alist):
+  result = alist[0]
+  for i in alist:
+    isitsmall = True 
+    for j in alist:
+      if i > j:
+        isitsmall = False
+    if isitsmall:
+      result = i 
+  return result
+  #print(result)
+  
+print(findMin([5,4,2,1,0]))
