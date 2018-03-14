3d82dbdcbec70587a75d21f3a8f014835142fc00
Julius Nyule <jlsnyule@yahoo.com>
2018-03-14 12:10:36 +0300

    refactor code

--- get_elements_sum.py
+++ get_elements_sum.py
@@ -9,6 +9,6 @@ def getsum(alist):
     return alist[0]
     
   else:
-    return alist[0] + getsum(alist[1:])
+    return alist[0] + getsum(alist[1:]) # Recursive - A recursive function is a function that calls itself.
     
 print(getsum([2, 4, 7]))
