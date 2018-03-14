a0de9fd1c13477ff921f4d5c681db53624eb035f
Julius Nyule <jlsnyule@yahoo.com>
2018-03-14 12:03:23 +0300

    add code comments

--- get_elements_sum.py
+++ get_elements_sum.py
@@ -1,3 +1,9 @@
+# sum of elements in a list
+"""
+The sum of the list alist is the sum of the first element of the list (alist[0]), 
+and the sum of the numbers in the rest of the list (alist[1:])
+"""
+
 def getsum(alist):
   if len(alist) == 1:
     return alist[0]
