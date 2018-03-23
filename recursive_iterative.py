8701a7ce727665af51d1dd7f727dfbf3428441c7
gtsofa <jlsnyule@yahoo.com>
2018-03-23 23:23:54 +0300

    add iterative_factorial.py

--- recursive_iterative.py
+++ recursive_iterative.py
@@ -11,3 +11,15 @@ def get_recursive_factorial(n):
         return n * get_recursive_factorial(n-1)
 
 print(get_recursive_factorial(6))
+
+def get_iterative_factorial(n):
+    if n < 0:
+        return -1
+
+    else:
+        fact = 1
+        for i in range(1, n+1):
+            fact *= i
+            
+        return fact
+print(get_iterative_factorial(6))
